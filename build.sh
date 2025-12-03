3781ca6dff09cac124a4a3f3dded033bbe42766c
Madhav Vedpathak <madhavve2@gmail.com>
2025-12-03 15:44:42 +0530

    Fix Render deployment: Upgrade pip and unpin mediapipe version

--- build.sh
+++ build.sh
@@ -2,9 +2,17 @@
 # Exit on error
 set -o errexit
 
+# Debug: Check Python environment
+echo "Python version:"
+python --version
+echo "Pip version:"
+pip --version
+
 # Build the frontend
 npm install
 npm run build
 
 # Install Python dependencies
+echo "Installing Python dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
